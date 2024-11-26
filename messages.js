88850e44928c35d95823da582f8ada2b6af07996
Alex Or <146792909+alejandro6022@users.noreply.github.com>
2024-11-25 21:36:49 -0600

    Store Messages Components

---
+++ messages.js
@@ -0,0 +1,22 @@
+/*message parts*/
+
+let actionVerbs = [
+    'Build', 'Organize', 'Create', 'Develop', 'Improve', 'Streamline', 'Achieve', 'Optimize', 'Plan', 'Execute'
+]
+
+let adjectives = [
+    'efficient', 'productive', 'innovative', 'focused', 'seamless', 'strategic', 'high-quality', 'impactful', 'dynamic', 'proactive'
+]
+
+let nouns = [
+    'workflow', 'solution', 'strategy', 'goal', 'system', 'team', 'vision', 'framework', 'process', 'initiative'
+]
+
+let adverbs = [
+    'efficienly', 'quickly', 'creatively', 'proactively', 'seamlessly', 'strategically', 'consistenly', 'effectively', 'diligently', 'resourcefully'
+]
+
+let contexts = [
+    'in your daily routine', 'while collaborating with your team', 'for long-term growth', 'to achieve your goals', 'in high-pressure situations', 'when planning for success',
+    'to drive innovation', 'for personal improvement'
+]
